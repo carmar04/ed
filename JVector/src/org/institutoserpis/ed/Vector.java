ce5219fc6cce41b55b1ece6ea5feeb47864a5694
carmar04 <1dam.csoro@institutoserpis.org>
2017-11-14 10:24:46 +0100

    Añadido CHolaMundo con monodevelop

--- JVector/src/org/institutoserpis/ed/Vector.java
+++ JVector/src/org/institutoserpis/ed/Vector.java
@@ -14,19 +14,35 @@ public class Vector {
 		
 		int position = indexOf(v, value);
 		System.out.print("position= " +position);
-		
+		int min = min(v);
+		System.out.println(" min= "+ min);
 
 	}
 	
 	public static int indexOf(int[] v,int value) {
+//		int index = 0;
+//		while(index < v.length && v[index] != value)//cuidado con la condición
+//			index++;
+//		if(index == v.length)
+//			index = -1;
+//			return index;
+//		
+//		for(int index = 0;index < v.length;index++)
+//			if(v[index] == value)
+//				return index;
+//	return -1;
+		
 		int index = 0;
-		while(v[index] != value && index < v.length)
+		while( index < v.length && v[index] != value)
 			index++;
-		if(index == v.length)
-			index = -1;
-			return index;
-		
-		
+		return index < v.length ? index : -1;
 	}
-
+	public static int min(int[] v) {
+	         int iMenor = v[0];
+	         for(int numero=1;numero<v.length;numero++){
+	             if(v[numero]<iMenor)
+	                 iMenor = v[numero];
+	         }
+	         return iMenor;
+	}
 }
